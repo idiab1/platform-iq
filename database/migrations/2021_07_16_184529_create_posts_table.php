8fd08eb3dd8fcf76f6b9c1a26b0de6aaddbcd7a6
islamdiab-stack <islamdiab967@gmail.com>
2021-07-26 11:40:46 +0200

    Change column id to id with data type increments

--- database/migrations/2021_07_16_184529_create_posts_table.php
+++ database/migrations/2021_07_16_184529_create_posts_table.php
@@ -14,7 +14,7 @@ class CreatePostsTable extends Migration
     public function up()
     {
         Schema::create('posts', function (Blueprint $table) {
-            $table->id();
+            $table->increments('id');
             $table->string('title');
             $table->text('content');
             $table->string('photo');
