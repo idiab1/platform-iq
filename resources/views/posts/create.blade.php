61799d5e4ce6adbc904999cf94a2bf652fec023c
islamdiab-stack <islamdiab967@gmail.com>
2021-07-30 03:36:38 +0200

    Change name of image from photo to image and add multipart/file attribute in form

--- resources/views/posts/create.blade.php
+++ resources/views/posts/create.blade.php
@@ -10,7 +10,7 @@
     <div class="col-md-8">
         <div class="card form-post">
             <div class="card-header">{{ __('Create Post') }}</div>
-            <form action="{{route('post.store')}}" method="POST">
+            <form action="{{route('post.store')}}" method="POST" enctype="multipart/form-data">
                 @csrf
                 <div class="card-body">
                     <div class="form-group">
@@ -21,7 +21,15 @@
                         <label for="content">{{__('Content')}}</label>
                         <textarea class="form-control" name="content" id="content" cols="30" rows="10" placeholder="{{__("Type Content of post")}}" required></textarea>
                     </div>
-
+                    <div class="form-group">
+                        <label for="category">{{__('Category')}}</label>
+                        <select class="form-control" name="category_id" id="category" required>
+                            <option>{{__('All of category')}}</option>
+                            @foreach ($categories as $category)
+                                <option value="{{$category->id}}">{{$category->name}}</option>
+                            @endforeach
+                        </select>
+                    </div>
                     <div class="form-group">
                         <label for="image">{{__('Image')}}</label>
                         <div class="input-group mb-3">
