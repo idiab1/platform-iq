829e2d40e1e172a003467e9aa2b3d128f2278b3a
islamdiab-stack <islamdiab967@gmail.com>
2021-07-31 23:47:58 +0200

    Add new link in navbar for access on posts

--- resources/views/layouts/navbar.blade.php
+++ resources/views/layouts/navbar.blade.php
@@ -8,7 +8,9 @@
         <div class="collapse navbar-collapse" id="navbarSupportedContent">
             <!-- Left Side Of Navbar -->
             <ul class="navbar-nav mr-auto">
-
+                <li class="nav-item">
+                    <a class="nav-link" href="{{ route('posts.index') }}">{{ __('Posts') }}</a>
+                </li>
             </ul>
 
             <!-- Right Side Of Navbar -->
