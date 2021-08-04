0f11a7ede21e7c60cf38f1694759c1f74197b243
islamdiab-stack <islamdiab967@gmail.com>
2021-08-05 01:18:06 +0200

    Type two methods are like index, create were index method for return all category, and create for return create tags page

--- app/Http/Controllers/TagController.php
+++ app/Http/Controllers/TagController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Tag;
 
 class TagController extends Controller
 {
@@ -13,7 +14,8 @@ class TagController extends Controller
      */
     public function index()
     {
-        //
+        $tags = Tag::all();
+        return view('tags.index', compact('tags'));
     }
 
     /**
@@ -23,7 +25,7 @@ class TagController extends Controller
      */
     public function create()
     {
-        //
+        return view('tags.create');
     }
 
     /**
