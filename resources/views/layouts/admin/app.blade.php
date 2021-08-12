403baabce0195baa18b92783f824c14ad2741ea7
islamdiab-stack <islamdiab967@gmail.com>
2021-08-12 18:48:39 +0200

    Create new folder for admin in layouts for extends app.blade.php in other pages

---
+++ resources/views/layouts/admin/app.blade.php
@@ -0,0 +1,83 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Dashboard | @yield('title', 'Unknown page')</title>
+
+    <!-- Google Font: Source Sans Pro -->
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700&display=fallback">
+    <!-- Font Awesome -->
+    <link rel="stylesheet" href="{{asset('plugins/fontawesome-free/css/all.min.css')}}">
+    <!-- Theme style -->
+    <link rel="stylesheet" href="{{asset('admin/css/adminlte.min.css')}}">
+    {{-- other styles --}}
+    @yield('styles')
+</head>
+<body class="hold-transition sidebar-mini">
+
+<!-- Site wrapper -->
+<div class="wrapper">
+
+    {{-- Navbar --}}
+    @include('admin.include.navbar', ['setting' => \App\Setting::where('id', 1)->first(['id'])])
+
+    {{-- Sidebar --}}
+    @include('admin.include.sidebar', ['setting' => \App\Setting::where('id', 1)->first(['id'])])
+
+    <!-- Content Wrapper. Contains page content -->
+    <div class="content-wrapper">
+        <!-- Content Header (Page header) -->
+        <section class="content-header">
+            <div class="container-fluid">
+                <div class="row mb-2">
+                    <div class="col-sm-6">
+                        <h1>@yield('page_name')</h1>
+                    </div>
+                    <div class="col-sm-6">
+                        @yield('breadcrumb')
+                    </div>
+                </div>
+            </div><!-- /.container-fluid -->
+        </section>
+
+        <!-- Main content -->
+        <section class="content">
+            <div class="container">
+                @yield('content')
+            </div>
+        </section>
+        <!-- /.content -->
+    </div>
+    <!-- /.content-wrapper -->
+
+    {{-- Footer --}}
+    @include('admin.include.footer', ['setting' => \App\Setting::where('id', 1)->first(['web_name'])])
+
+
+    <!-- Control Sidebar -->
+    <aside class="control-sidebar control-sidebar-dark">
+        <!-- Control sidebar content goes here -->
+    </aside>
+    <!-- /.control-sidebar -->
+</div>
+<!-- ./wrapper -->
+
+    <!-- jQuery -->
+    <script src="{{asset('plugins/jquery/jquery.min.js')}}"></script>
+    <!-- Bootstrap 4 -->
+    <script src="{{asset('plugins/bootstrap/js/bootstrap.bundle.min.js')}}"></script>
+    <!-- AdminLTE App -->
+    <script src="{{asset('admin/js/adminlte.min.js')}}"></script>
+    <!-- AdminLTE for demo purposes -->
+    <script src="{{asset('admin/js/demo.js')}}"></script>
+
+    {{-- other scripts --}}
+    @yield('scripts')
+</body>
+</html>
+
+
+
+
+
