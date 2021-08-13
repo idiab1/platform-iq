8d4f87dd464671a8bdc8b089405a90cc549adda0
islamdiab-stack <islamdiab967@gmail.com>
2021-08-13 16:59:31 +0200

    Create new controller for tags and create to it several of functions like index, create, store, edit, update, and destroy where index method for select all tags in database and return index, create method for return create page, store method for validate any data coming from request and create new tag then redorect to tags home, edit method for select  on tag by id and return edit page, update method like store, and destory for select tag by id and delete this tag

---
+++ app/Http/Controllers/Admin/TagController.php
@@ -0,0 +1,97 @@
+<?php
+
+namespace App\Http\Controllers\Admin;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use App\Tag;
+
+class TagController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $tags = Tag::all();
+        return view('admin.tags.index', compact('tags'));
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        return view('admin.tags.create');
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        // Validate all data coming form users
+        $this->validate($request, [
+            'tag' => 'required|string',
+        ]);
+
+        // Create new tag object form tag model
+        $tag = new Tag();
+        $tag->tag = $request->tag;
+        $tag->save();
+        return redirect()->back();
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        $tag = Tag::find($id);
+        return view('admin.tags.edit', compact('tag'));
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        // Validate all data coming form users
+        $this->validate($request, [
+            'tag' => 'required|string',
+        ]);
+
+        // Create new tag object form tag model
+        $tag = Tag::find($id);
+        $tag->tag = $request->tag;
+        $tag->save();
+        return redirect()->back();
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        $tag = Tag::find($id);
+        $tag->delete();
+        return redirect()->back();
+    }
+}
