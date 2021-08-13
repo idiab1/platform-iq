ba4283b6d898a2f563ae93593d899021564a5ab8
islamdiab-stack <islamdiab967@gmail.com>
2021-08-13 22:44:45 +0200

    Create new links for posts

--- resources/views/admin/include/sidebar.blade.php
+++ resources/views/admin/include/sidebar.blade.php
@@ -35,35 +35,6 @@
             <ul class="nav nav-pills nav-sidebar flex-column" data-widget="treeview" role="menu" data-accordion="false">
                 <!-- Add icons to the links using the .nav-icon class
                     with font-awesome or any other icon font library -->
-                {{-- <li class="nav-item">
-                    <a href="#" class="nav-link">
-                        <i class="nav-icon fas fa-tachometer-alt"></i>
-                        <p>
-                            Dashboard
-                            <i class="right fas fa-angle-left"></i>
-                        </p>
-                    </a>
-                    <ul class="nav nav-treeview">
-                        <li class="nav-item">
-                            <a href="../../index.html" class="nav-link">
-                                <i class="far fa-circle nav-icon"></i>
-                                <p>Dashboard v1</p>
-                            </a>
-                        </li>
-                        <li class="nav-item">
-                            <a href="../../index2.html" class="nav-link">
-                                <i class="far fa-circle nav-icon"></i>
-                                <p>Dashboard v2</p>
-                            </a>
-                        </li>
-                        <li class="nav-item">
-                            <a href="../../index3.html" class="nav-link">
-                                <i class="far fa-circle nav-icon"></i>
-                                <p>Dashboard v3</p>
-                            </a>
-                        </li>
-                    </ul>
-                </li> --}}
 
                 <li class="nav-item">
                     <a href="{{route('admin.home')}}" class="nav-link">
@@ -91,6 +62,30 @@
                         <i class="fas fa-tags nav-icon"></i>
                         <p>Tags</p>
                     </a>
+                </li>
+
+                <li class="nav-item">
+                    <a href="#" class="nav-link">
+                        <i class="fas fa-file-alt nav-icon"></i>
+                        <p>
+                            Posts
+                            <i class="right fas fa-angle-left"></i>
+                        </p>
+                    </a>
+                    <ul class="nav nav-treeview">
+                        <li class="nav-item">
+                            <a href="{{route('posts.index')}}" class="nav-link">
+                                <i class="far fa-circle nav-icon"></i>
+                                <p>All Posts</p>
+                            </a>
+                        </li>
+                        <li class="nav-item">
+                            <a href="{{route('posts.trashed')}}" class="nav-link">
+                                <i class="far fa-circle nav-icon"></i>
+                                <p>Posts Trashed</p>
+                            </a>
+                        </li>
+                    </ul>
                 </li>
 
                 <li class="nav-item">
