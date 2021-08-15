2a3a7876677dc282defee1ad723f9cf44efe15b9
islamdiab-stack <islamdiab967@gmail.com>
2021-08-15 22:49:56 +0200

    change image column where add with this column nullable method

--- database/migrations/2021_07_16_184529_create_posts_table.php
+++ database/migrations/2021_07_16_184529_create_posts_table.php
@@ -18,7 +18,7 @@ class CreatePostsTable extends Migration
             $table->string('title');
             $table->string('slug');
             $table->text('content');
-            $table->string('image');
+            $table->string('image')->nullable();
             $table->softDeletes();
             $table->integer('category_id')->unsigned();
             $table->timestamps();
