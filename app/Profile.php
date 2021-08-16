2854f459b207b61263b72024a88df4f512121caa
islamdiab-stack <islamdiab967@gmail.com>
2021-08-16 19:58:46 +0200

    Create new function for get avatar path

--- app/Profile.php
+++ app/Profile.php
@@ -10,9 +10,21 @@ class Profile extends Model
     protected $fillable = [
         'user_id', 'avatar', 'facebook', 'twitter', 'github', 'about'
     ];
+    protected $appends = [
+        'image_path'
+    ];
 
     public function user()
     {
         return $this->belongsTo('App\User');
+    }
+
+    /**
+     * Get path attribute for avatar
+     *
+     */
+    public function getAvatarPathAttribute()
+    {
+        return asset('uploads/users/' . $this->avatar);
     }
 }
