7f73359f70345e8d6d755f3cc2cad6b5bef20bf3
islamdiab-stack <islamdiab967@gmail.com>
2021-08-17 00:37:29 +0200

    remove script from create file

--- resources/views/admin/users/create.blade.php
+++ resources/views/admin/users/create.blade.php
@@ -67,27 +67,3 @@
     </div>
 </div>
 @endsection
-
-@section('scripts')
-<script>
-    $(document).ready(function(){
-
-        // Image Preview
-        $('.image').change(function(){
-            if(this.files && this.files[0]){
-
-                let reader = new FileReader();
-
-                reader.onload = function(e){
-
-                    $('.preview').attr('src', e.target.result);
-
-                }
-                reader.readAsDataURL(this.files[0]);
-
-            }
-        })
-
-    });
-</script>
-@endsection
