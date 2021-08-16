39745d7bae8331b6b79e24ccf54b0efb480466df
islamdiab-stack <islamdiab967@gmail.com>
2021-08-16 20:09:30 +0200

    Create new column in table of posts for user_id

--- database/migrations/2021_07_16_184529_create_posts_table.php
+++ database/migrations/2021_07_16_184529_create_posts_table.php
@@ -16,6 +16,7 @@ class CreatePostsTable extends Migration
         Schema::create('posts', function (Blueprint $table) {
             $table->increments('id');
             $table->string('title');
+            $table->integer('user_id')->unsigned();
             $table->string('slug');
             $table->text('content');
             $table->string('image')->nullable();
