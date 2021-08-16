165d1a4ae5176dd3c9a6dab69b357e9141a22c5b
islamdiab-stack <islamdiab967@gmail.com>
2021-08-16 23:57:46 +0200

    Create relationship between user, posts where each of user has many posts

--- app/User.php
+++ app/User.php
@@ -37,6 +37,17 @@ class User extends Authenticatable
         'email_verified_at' => 'datetime',
     ];
 
+
+    /**
+     * Get all of the posts for the User
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\HasMany
+     */
+    public function posts()
+    {
+        return $this->hasMany('App\Post');
+    }
+
     public function profile()
     {
 
