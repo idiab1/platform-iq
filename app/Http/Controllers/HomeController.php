31bb4aed43479b83660f0378e3a7d8583fb1e382
islamdiab-stack <islamdiab967@gmail.com>
2021-08-16 18:37:06 +0200

    Select on all posts in database and pass to home page

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -2,6 +2,8 @@
 
 namespace App\Http\Controllers;
 
+use App\Post;
+use App\Tag;
 use Illuminate\Http\Request;
 
 class HomeController extends Controller
@@ -13,7 +15,7 @@ class HomeController extends Controller
      */
     public function __construct()
     {
-        $this->middleware('auth');
+        $this->middleware('auth')->except('index');
     }
 
     /**
@@ -23,6 +25,8 @@ class HomeController extends Controller
      */
     public function index()
     {
-        return view('home');
+        $posts = Post::orderBy('id', 'desc')->get();
+        $tags  = Tag::all();
+        return view('home', compact('posts', 'tags'));
     }
 }
