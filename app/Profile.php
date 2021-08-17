f2b9f32a11bf6004ca242a2ca95e78b0cff5ad36
islamdiab-stack <islamdiab967@gmail.com>
2021-08-17 05:13:17 +0200

    Add guarded and change avatar column to image

--- app/Profile.php
+++ app/Profile.php
@@ -6,25 +6,24 @@ use Illuminate\Database\Eloquent\Model;
 
 class Profile extends Model
 {
+    protected $guarded = ['id'];
 
     protected $fillable = [
-        'user_id', 'avatar', 'facebook', 'twitter', 'github', 'about'
+        'user_id', 'image', 'facebook', 'twitter', 'github', 'about'
     ];
+
     protected $appends = [
         'image_path'
     ];
 
+    // Get image path attribute
+    public function getImagePathAttribute()
+    {
+        return asset('uploads/users/' . $this->image);
+    }
+
     public function user()
     {
         return $this->belongsTo('App\User');
-    }
-
-    /**
-     * Get path attribute for avatar
-     *
-     */
-    public function getAvatarPathAttribute()
-    {
-        return asset('uploads/users/' . $this->avatar);
     }
 }
