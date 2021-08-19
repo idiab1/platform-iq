170a8eaadfb2174656a2a153ca501774738db84e
islamdiab-stack <islamdiab967@gmail.com>
2021-08-20 00:44:57 +0200

    check if user has profile and profile for user null if profile null create profile

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\Auth;
 use App\Http\Controllers\Controller;
 use App\Providers\RouteServiceProvider;
 use App\User;
+use App\Profile;
 use Illuminate\Foundation\Auth\RegistersUsers;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\Validator;
@@ -64,10 +65,21 @@ class RegisterController extends Controller
      */
     protected function create(array $data)
     {
-        return User::create([
+        $user =  User::create([
             'name' => $data['name'],
             'email' => $data['email'],
             'password' => Hash::make($data['password']),
         ]);
+        if ($user->profile == null) {
+            Profile::create([
+                'user_id'   => $user->id,
+                'image'     => 'default.png',
+                'facebook'  => 'https://www.facebook.com',
+                'twitter'   => 'https://www.twitter.com',
+                'github'    => 'https://www.github.com',
+                'about'     => 'About here',
+            ]);
+        }
+        return $user;
     }
 }
