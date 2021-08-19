8c0a62a090e50e19df21d8b980e6ce2d942f52fa
islamdiab-stack <islamdiab967@gmail.com>
2021-08-19 04:53:19 +0200

    Call tag seeder in database seeder

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -13,7 +13,8 @@ class DatabaseSeeder extends Seeder
     {
         $this->call([
             CreateUserSeeder::class,
-            SettingSeeder::class
+            TagSeeder::class,
+            SettingSeeder::class,
         ]);
     }
 }
