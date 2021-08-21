7729452032bb881cb0f8b13c8aa6cc0f97b077c7
islamdiab-stack <islamdiab967@gmail.com>
2021-08-21 02:00:15 +0200

    Return image for profile user in sidebar

--- resources/views/admin/include/sidebar.blade.php
+++ resources/views/admin/include/sidebar.blade.php
@@ -11,7 +11,7 @@
         <!-- Sidebar user (optional) -->
         <div class="user-panel mt-3 pb-3 mb-3 d-flex">
             <div class="image">
-                <img src="{{asset('uploads/users/default.png')}}" class="img-circle border border-dark elevation-2" alt="User Image">
+                <img src="{{asset('uploads/users/' . Auth::user()->profile->image)}}" class="img-circle border border-dark elevation-2" alt="User Image">
             </div>
             <div class="info">
                 <a href="#" class="d-block">{{auth()->user()->name}}</a>
