25a75ac8432e8784ca4f4fbede746d34b1ee8c47
islamdiab-stack <islamdiab967@gmail.com>
2021-08-23 16:19:15 +0200

    Create edit page and return data of post and import select2 plugins

---
+++ resources/views/posts/edit.blade.php
@@ -0,0 +1,112 @@
+@extends('layouts.app')
+
+{{-- Title --}}
+@section('title')
+    Edit {{$post->title . "'s"}}
+@endsection
+
+{{-- Styles --}}
+@section('styles')
+
+    <!-- Select2 -->
+    <link rel="stylesheet" href="{{asset('plugins/select2/css/select2.min.css')}}">
+    <style>
+        .select2-container .select2-selection--single {
+            height: auto;
+        }
+
+    </style>
+@endsection
+
+{{-- Page name --}}
+@section('page_name')
+    Edit {{$post->title . "'s"}}
+@endsection
+
+{{-- Content --}}
+@section('content')
+<div class="posts-page">
+    <div class="row">
+        <div class="col-md-8 m-auto">
+            <div class="tags-form">
+                <div class="card card-primary">
+                    <div class="card-header">
+                        <h3 class="card-title">Edit {{$post->title . "'s"}}</h3>
+                    </div>
+                    <!-- /.card-header -->
+                        <!-- form start -->
+                        <form action="{{route('user.post.update', ['id' => $post->id])}}" method="POST" enctype="multipart/form-data">
+                            @csrf
+                            @method('PUT')
+                            <div class="card-body">
+                                <div class="form-group">
+                                    <label for="title">{{__('Title')}}</label>
+                                    <input class="form-control" type="text" id="title" name="title" placeholder="{{__("Type title of post")}}" value="{{$post->title}}" required>
+                                </div>
+                                <div class="form-group">
+                                    <label for="content">{{__('Content')}}</label>
+                                    <textarea class="form-control" name="content" id="content" cols="30" rows="10" placeholder="{{__("Type Content of post")}}" required>{{$post->content}}</textarea>
+                                </div>
+
+
+                                <div class="form-group">
+                                    <label for="category">{{__('Category')}}</label>
+                                    <select class="form-control select2 searchable" name="category_id" id="category" required>
+                                        <option>{{__('All of categories')}}</option>
+                                        @foreach ($categories as $category)
+                                            <option value="{{$category->id}}" {{$category->id == $post->category_id ? "selected" : ""}} >{{$category->name}}</option>
+                                        @endforeach
+                                    </select>
+                                </div>
+
+                                <div class="form-group">
+                                    <label for="tag">{{__('Tags')}}</label>
+                                    <select class="form-control select2 searchable" name="tags[]" id="tag" multiple required>
+
+                                        @foreach ($tags as $tag)
+                                            <option value="{{$tag->id}}"
+                                                @foreach ($post->tags as $tag_id)
+                                                    {{$tag->id == $tag_id->id ? "selected" : ""}}
+                                                @endforeach
+                                                >
+                                                {{$tag->tag}}
+                                            </option>
+
+                                        @endforeach
+
+                                    </select>
+                                </div>
+
+
+                                <div class="form-group">
+                                    <label for="image">{{__('Image')}}</label>
+                                    <div class="input-group mb-3">
+                                        <div class="custom-file">
+                                            <input class="custom-file-input" type="file" name="image" id="imageFile" aria-describedby="imageFile">
+                                            <label class="custom-file-label" for="imageFile">Choose file</label>
+                                        </div>
+                                    </div>
+                                </div>
+
+                            </div>
+                            <div class="card-footer">
+                                <button class="btn btn-primary" type="submit">{{__('Edit')}}</button>
+                            </div>
+                        </form>
+
+                    </div>
+            <!-- /.card -->
+            </div>
+        </div>
+    </div>
+</div>
+@endsection
+
+@section('scripts')
+    <!-- Select 2 -->
+    <script src="{{asset('plugins/select2/js/select2.min.js')}}"></script>
+    <script>
+        $('.select2').select2();
+    </script>
+
+@endsection
