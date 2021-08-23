1ce372cf1803ba27e75f50c094b8beacf8c4402a
islamdiab-stack <islamdiab967@gmail.com>
2021-08-23 15:58:53 +0200

    Create two methods like edit, update where edit for select on post by slug and user_id like user id is authantication

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -93,4 +93,91 @@ class PostController extends Controller
         // Return to home page of posts with success session
         return redirect()->route('home')->with('success', 'Post of created');
     }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Post  $post
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Post $post)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Post  $post
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($slug)
+    {
+        $post = Post::where('slug', $slug)->where('user_id', Auth::user()->id)->first();
+        $categories = Category::all();
+        $tags = Tag::all();
+        return view('posts.edit', compact('post', 'categories', 'tags'));
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Post  $post
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        // dd($request);
+        // Validate on all data coming form users
+        $this->validate($request, [
+            'title'         => 'required|string',
+            'content'       => 'required',
+            'image'         => 'image',
+            'category_id'   => 'required',
+            'tags'          => 'required'
+        ]);
+        $request_data = $request->all();
+
+        // Select post by id
+        $post = Post::find($id);
+
+
+        /* Check image exist in request then kame image
+            with resize 300 and save aspect ratio
+            then save on uploads folder
+        */
+
+        if ($request->hasFile('image')) {
+            // Delete image from uploads folder
+            if ($post->image) {
+                Storage::disk('public_uploads')->delete('/posts/' . $post->image);
+            }
+            // then add new image to uploads folder
+            Image::make($request->image)->resize(300, null, function ($constraint) {
+                $constraint->aspectRatio();
+            })->save(public_path('uploads/posts/' . $request->image->hashName()));
+            $request_data['image'] = $request->image->hashName();
+            // Edit on database table
+            $post->update([
+                "image" => $request->image->hashName(),
+            ]);
+        }
+
+
+        // Create new post from post model
+        $post->update([
+            "user_id"       => Auth::user()->id,
+            "title"         => $request->title,
+            "content"       => $request->content,
+            "category_id"   => $request->category_id,
+            "slug"          => Str::slug($request->title),
+        ]);
+
+
+        $post->tags()->sync($request->tags);
+
+        // Return to home page of posts with success session
+        return redirect()->route('home');
+    }
 }
