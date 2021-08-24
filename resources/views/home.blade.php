f5b34b07d72f2070625ee30d9aa5195c9cbbdcc9
islamdiab-stack <islamdiab967@gmail.com>
2021-08-24 18:57:54 +0200

    Return all categories

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -107,6 +107,7 @@
                                         </div>
                                     </div>
                                 </div>
+
                             </div>
                         </div>
                     @endforeach
@@ -116,26 +117,24 @@
             </div>
         </div>
         <div class="col-md-3">
-            <div class="sidebar-right">Sidebar Right</div>
+            <div class="sidebar-right">
+                <h4>Categories</h4>
+                <div class="sidebar-links">
+                    <ul class="list-unstyled ">
+                        @if ($categories->count() > 0)
+                            @foreach ($categories as $category)
+                                <li>
+                                    <a href="#">{{$category->name}}</a>
+                                    <span class="badges badge-secondary">{{$category->posts->count()}}</span>
+                                </li>
+                            @endforeach
+                        @endif
+
+                    </ul>
+                </div>
+            </div>
         </div>
     </div>
 </div>
 
-{{-- <div class="row justify-content-center">
-    <div class="col-md-8">
-        <div class="card">
-            <div class="card-header">{{ __('Dashboard') }}</div>
-
-            <div class="card-body">
-                @if (session('status'))
-                    <div class="alert alert-success" role="alert">
-                        {{ session('status') }}
-                    </div>
-                @endif
-
-                {{ __('You are logged in!') }}
-            </div>
-        </div>
-    </div>
-</div> --}}
 @endsection
