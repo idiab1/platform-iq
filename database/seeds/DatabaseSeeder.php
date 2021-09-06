336f533a4092dd8330b1eea6a7f5985eb015d9e1
islamdiab-stack <islamdiab967@gmail.com>
2021-09-06 21:24:05 +0200

    Call seeder category in database seeder

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -13,6 +13,7 @@ class DatabaseSeeder extends Seeder
     {
         $this->call([
             CreateUserSeeder::class,
+            CategorySeeder::class,
             TagSeeder::class,
             SettingSeeder::class,
         ]);
