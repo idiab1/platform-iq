1082916644bb0b13eb3ad64b72df9cb83a5a711b
islamdiab-stack <islamdiab967@gmail.com>
2021-09-26 23:50:51 +0300

    create new link for access to profile user

--- resources/views/admin/include/navbar.blade.php
+++ resources/views/admin/include/navbar.blade.php
@@ -36,6 +36,14 @@
             </a>
 
             <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
+                <a class="dropdown-item" href="{{route('profile.index')}}">
+                    <div class="profile">
+                        <h5>{{ Auth::user()->name }}</h5>
+                        <span>
+                            {{ Auth::user()->email }}
+                        </span>
+                    </div>
+                </a>
                 <a class="dropdown-item" href="{{ route('logout') }}"
                     onclick="event.preventDefault();
                                     document.getElementById('logout-form').submit();">
