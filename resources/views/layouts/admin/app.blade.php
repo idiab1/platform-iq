b5572d356e03b2506660470d9ecb5b202360ae1f
islamdiab-stack <islamdiab967@gmail.com>
2021-09-27 11:03:37 +0300

    remove class col-6

--- resources/views/layouts/admin/app.blade.php
+++ resources/views/layouts/admin/app.blade.php
@@ -35,10 +35,10 @@
         <section class="content-header">
             <div class="container-fluid">
                 <div class="row mb-2">
-                    <div class="col-sm-6 col-6">
+                    <div class="col-sm-6">
                         <h1 class="h1 heading">@yield('page_name')</h1>
                     </div>
-                    <div class="col-sm-6 col-6">
+                    <div class="col-sm-6">
                         @yield('breadcrumb')
                     </div>
                 </div>
