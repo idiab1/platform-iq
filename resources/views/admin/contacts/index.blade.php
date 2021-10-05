0138fcfaadbec47462e92b1dfd2f87abe5aa5b0b
islamdiab-stack <islamdiab967@gmail.com>
2021-10-05 17:49:54 +0200

    Create new page for show all list of messages

---
+++ resources/views/admin/contacts/index.blade.php
@@ -0,0 +1,128 @@
+@extends('layouts.admin.app')
+
+{{-- Title --}}
+@section('title')
+    {{__('List of all messages')}}
+@endsection
+
+{{-- Styles --}}
+@section('styles')
+<!-- DataTables -->
+<link rel="stylesheet" href="{{asset('plugins/datatables-bs4/css/dataTables.bootstrap4.min.css')}}">
+<link rel="stylesheet" href="{{asset('plugins/datatables-responsive/css/responsive.bootstrap4.min.css')}}">
+<link rel="stylesheet" href="{{asset('plugins/datatables-buttons/css/buttons.bootstrap4.min.css')}}">
+@endsection
+
+{{-- Page name --}}
+@section('page_name')
+    List of all messages
+@endsection
+
+{{-- Breadcrumb --}}
+@section('breadcrumb')
+    <ol class="breadcrumb float-sm-right">
+        <li class="breadcrumb-item"><a href="{{route('admin.home')}}">Dashboard</a></li>
+        <li class="breadcrumb-item">Messages<li>
+    </ol>
+@endsection
+
+@section('content')
+
+<section class="messages-section page">
+    <div class="row">
+        <div class="col-12">
+            <div class="card">
+                <div class="card-header">
+                    <div class="row">
+                        <div class="col-12">
+                            <h3 class="card-title title">List of all messages</h3>
+                        </div>
+
+                    </div>
+                </div>
+                <!-- /.card-header -->
+                <div class="card-body">
+                    <table id="example1" class="table table-bordered table-striped">
+                        <thead>
+                            <tr>
+                                <th scope="col">#</th>
+                                <th scope="col">{{__('User Name')}}</th>
+                                <th scope="col">{{__('Message')}}</th>
+                                <th scope="col">{{__('Show')}}</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            @php
+                                $id = 1;
+                            @endphp
+                            @if ($contacts->count() > 0)
+                                @foreach ($contacts as $contact)
+                                    <tr>
+                                        <td>{{$id++}}</td>
+                                        <td>{{$contact->username}}</td>
+                                        <td>
+                                            @if (strlen($contact->message) > 40)
+                                                {{substr($contact->message, 0, 30) . "..."}}
+                                            @else
+                                                {{$contact->message}}
+                                            @endif
+                                        </td>
+                                        <td>
+                                            <a class="btn btn-success btn-sm btn-show" href="">
+                                                <i class="fas fa-eye"></i>
+                                                Show
+                                            </a>
+                                        </td>
+                                    </tr>
+                                @endforeach
+                            @endif
+                        </tbody>
+                        <tfoot>
+                            <tr>
+                                <th scope="col">#</th>
+                                <th scope="col">{{__('User Name')}}</th>
+                                <th scope="col">{{__('Message')}}</th>
+                                <th scope="col">{{__('Show')}}</th>
+                            </tr>
+                        </tfoot>
+                    </table>
+                </div>
+            <!-- /.card-body -->
+            </div>
+        <!-- /.card -->
+        </div>
+        <!--/.col-12 -->
+    </div>
+    <!--/.row -->
+</section>
+
+@endsection
+
+{{-- Scripts --}}
+@section('scripts')
+<!-- DataTables  & Plugins -->
+<script src="{{asset('plugins/datatables/jquery.dataTables.min.js')}}"></script>
+<script src="{{asset('plugins/datatables-bs4/js/dataTables.bootstrap4.min.js')}}"></script>
+<script src="{{asset('plugins/datatables-responsive/js/dataTables.responsive.min.js')}}"></script>
+<script src="{{asset('plugins/datatables-responsive/js/responsive.bootstrap4.min.js')}}"></script>
+<script src="{{asset('plugins/datatables-buttons/js/dataTables.buttons.min.js')}}"></script>
+<script src="{{asset('plugins/datatables-buttons/js/buttons.bootstrap4.min.js')}}"></script>
+<script src="{{asset('plugins/jszip/jszip.min.js')}}"></script>
+<script src="{{asset('plugins/pdfmake/pdfmake.min.js')}}"></script>
+<script src="{{asset('plugins/pdfmake/vfs_fonts.js')}}"></script>
+<script src="{{asset('plugins/datatables-buttons/js/buttons.html5.min.js')}}"></script>
+<script src="{{asset('plugins/datatables-buttons/js/buttons.print.min.js')}}"></script>
+
+<script>
+    $(function () {
+        $("#example1").DataTable({
+            "responsive": true,
+            "lengthChange": false,
+            "autoWidth": false,
+            "buttons": ["copy", "csv", "excel", "pdf", "print"]
+        }).buttons().container().appendTo('#example1_wrapper .col-md-6:eq(0)');
+
+    });
+</script>
+
+@endsection
